7e2b07b016385a0890ebb061b0f0adfe2b0f40c9
Perseus-D <pereus@62:f6:04:ec:ca:d2.home>
2025-11-20 18:29:56 -0500

    Task 1.1

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,30 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private final String playID;
+    private final int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
+    }
+
+    /**
+     * Return the play ID of this performance.
+     */
+    @SuppressWarnings({"checkstyle:JavadocMethod", "checkstyle:SuppressWarnings"})
+    public String getPlayID() {
+        return this.playID;
+    }
+
+    /**
+     * Return the audience size for this performance.
+     */
+    public int getAudience() {
+        return this.audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,34 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
-    public Play(String name, String type) {
+    public Play(String nuck, String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Return the name of this play.
+     *
+     * @return the name of this play
+     */
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Return the type of this play.
+     *
+     * @return the type of this play
+     */
+    public String getType() {
+        return this.type;
     }
 }
